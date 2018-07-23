06940cccb29ea6b21665dd512421d0ec65fb4ec6
Otabek Kamalov <otakam86@gmail.com>
2018-07-23 18:46:20 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -89,7 +89,12 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+ holiday_hash.each do |key1,value1|
+   value1.each do ||
+     
+   end
+   
+ end
 end
 
 
